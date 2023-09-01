91df26742bd16050ea654b2ece1be3750cf94401
BarriBarri20 <akrambenghanem76@gmail.com>
2023-09-01 23:10:53 +0100

    initialization of the vuejs project

--- vuej-with-drf/front/src/App.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-  <div class="m-8">
-    <h1 class="container-lg text-pink-900 text-center">Vue 3 + Vite + TypeScript + Tailwind CSS</h1>
-  </div>
-</template>
